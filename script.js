2567eeb92ea53293ce3268c9aa4117ea1eaa3bf0
berry <kathborowy@gmail.com>
2022-12-05 11:31:16 +0100

    placing xs on a board

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let activePlayer = 'player1'
+
+const Board = (() => {
+    let boardArr = ["", "", "", "", "", "", "", "", ""];
+    const returnBoardArr = () => {return boardArr}
+    return {
+        returnBoardArr
+    }
+})()
+
 const GameFlow = (() => {
-    
+    let boardDiv = document.querySelectorAll('div.boardPiece');
+    boardDiv.forEach(div => {
+        div.addEventListener('click', (e) => {
+            assignToArr(e.target.getAttribute('data-index'))
+            placeChar(e.target)
+        })
+    })
+
+    const assignToArr = (i) => {
+        Board.returnBoardArr()[i] = 'x'
+        console.log(Board.returnBoardArr())
+        
+    }
+    const placeChar = (el) => {
+        el.textContent = 'x'
+    }
     const switchPlayers = () => {
         if (activePlayer === 'player1') {
             document.getElementById('player1Btn').classList.remove('activeButton');
@@ -26,3 +50,4 @@ btns.forEach(button => {
         GameFlow.switchPlayers();
     })
 }) 
+
