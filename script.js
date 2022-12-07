d5d4b8cfc542af0e3677256f28c64481fc953730
berry <kathborowy@gmail.com>
2022-12-07 16:15:28 +0100

    pickAWinner module

--- script.js
+++ script.js
@@ -59,6 +59,35 @@ const GameFlow = (() => {
     }
 })()
 
-const winningConstitions = () => ({
-
+const pickAWinner = ((scoreArray, i, sign) => {
+    const winnerIndexes = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    const seeIfPlayerWon = () => {
+        let indexesWCurrentSign = [];
+        winnerIndexes.filter(arr => {
+            if (arr.includes(i)) indexesWCurrentSign.push(arr)
+        });
+        for (let indexWCurrentSign of indexesWCurrentSign) {
+            let copy = indexWCurrentSign;
+            for (let i = indexWCurrentSign.length - 1; i >= 0; i--) {
+              let index = indexWCurrentSign[i]
+              if (scoreArray[index] === sign) {
+                copy.splice(i, 1) 
+              }
+            }
+            if (copy.length === 0) return 'done'
+        }
+    }
+    return {
+        seeIfPlayerWon
+    }
+     
 })()
